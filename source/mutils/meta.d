5f2579a42bf86795234bb86a4cadd16d0201a74f
mmcomando <mmcomando@gmail.com>
2017-07-15 14:16:35 +0200

    Add compile time map (CPMap) to containers

---
+++ source/mutils/meta.d
@@ -0,0 +1,42 @@
+﻿module mutils.meta;
+
+import std.meta;
+
+template removeEven(Arr...){
+	static if(Arr.length>2){
+		alias removeEven=AliasSeq!(Arr[1], removeEven!(Arr[2..$]));
+	}else static if(Arr.length==2){
+		alias removeEven=AliasSeq!(Arr[1]);
+	}else{
+		alias removeEven=AliasSeq!();
+	}
+}
+
+unittest{
+	static assert(is(removeEven!(int, float, long)==AliasSeq!(float)));
+	static assert(is(removeEven!(int, float, long, float)==AliasSeq!(float, float)));
+}
+
+
+template removeOdd(Arr...){
+	static if(Arr.length>2){
+		alias removeOdd=AliasSeq!(Arr[0], removeOdd!(Arr[2..$]));
+	}else static if(Arr.length==2){
+		alias removeOdd=AliasSeq!(Arr[0]);
+	}else{
+		alias removeOdd=Arr;
+	}
+}
+
+unittest{
+	static assert(is(removeOdd!(int, float)==AliasSeq!(int)));
+	static assert(is(removeOdd!(int, float, long)==AliasSeq!(int, long)));
+}
+
+template getType(alias T){
+	alias getType=typeof(T);
+}
+
+unittest{
+	static assert(is(getType!(6)==int));
+}
