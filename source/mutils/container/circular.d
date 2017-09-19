de69d7d75c476d2750c4c76fed41c391d17447e2
mmcomando <mmcomando@gmail.com>
2017-09-20 00:32:43 +0200

    Circular container

---
+++ source/mutils/container/circular.d
@@ -0,0 +1,33 @@
+﻿module mutils.container.circular;
+
+struct Circular(T){
+	T[] range;
+	size_t currElement;
+
+	this(T[] r){
+		assert(r.length>0);
+		range=r;
+	}
+
+	ref T get(){
+		return range[currElement];
+	}
+
+	ref T popFront(){
+		currElement++;
+		if(currElement>=range.length){
+			currElement=0;
+		}
+		return get();
+	}
+
+	ref T popBack(){
+		if(currElement==0){
+			currElement=range.length;
+		}
+		currElement--;
+		return get();
+	}
+
+}
+
