da6c9da25957fa194a7a461e09a2967723b8ac24
mmcomando <mmcomando@gmail.com>
2017-10-16 08:43:16 +0200

    Linear algebra vecor + algorithm

---
+++ source/mutils/linalg/vec.d
@@ -0,0 +1,107 @@
+﻿//Package for test, maybe there will be full implementation
+module mutils.linalg.vec;
+
+
+
+struct Vec(T, int dim){
+	static assert(dim>0);
+	enum dimension=dim;
+
+	union{
+		T[dim] vector;
+
+		static if(dim==2){
+			struct{
+				T x;
+				T y;
+			}
+			struct{
+				T w;
+				T h;
+			}
+		}
+
+		static if(dim==3){
+			struct{
+				T x;
+				T y;
+				T z;
+			}
+			struct{
+				T w;
+				T h;
+			}
+		}
+	}
+
+	this(Args... )(Args values){
+		static assert(Args.length==dim || Args.length==1);
+		
+		static if(Args.length==1){
+			vector[]=values[0];
+		}else{
+			foreach(i, val; values){
+				vector[i]=values[i];
+			}
+		}
+	}
+
+	size_t length(){
+		return vector.length;
+	}
+
+
+	void opAssign(T[dim] rhs){
+		vector=rhs;
+	}
+
+	Vec!(T, dim) opBinary(string op)(Vec!(T, dim) rhs)
+	{
+		Vec!(T, dim) ret;
+		mixin("ret.vector=this.vector[] "~op~" rhs.vector[];");
+		return ret;
+	}
+
+	Vec!(T, dim) opBinary(string op)(T[dim] rhs)
+	{
+		Vec!(T, dim) ret;
+		mixin("ret.vector=this.vector[] "~op~" rhs[];");
+		return ret;
+	}
+
+	Vec!(T, dim) opBinary(string op)(float rhs)
+	{
+		Vec!(T, dim) ret=this;
+		foreach(ref v;ret.vector){
+			mixin("v=cast(T)(v "~op~" rhs);");
+			
+		}
+		return ret;
+	}
+
+	Vec!(T, dim) opBinary(string op)(double rhs)
+	{
+		Vec!(T, dim) ret=this;
+		foreach(ref v;ret.vector){
+			mixin("v=cast(T)(v "~op~" rhs);");
+			
+		}
+		return ret;
+	}
+
+
+}
+
+unittest{
+	int[2] arr=[1,2];
+	alias vec2i=Vec!(int, 2);
+	vec2i v1=vec2i(1, 1);
+	vec2i v2=vec2i(3);
+	vec2i v3=arr;
+	assert(v2==vec2i(3, 3));
+	v1.opBinary!("+")(v2);
+	assert((v1+v2)==vec2i(4, 4));
+	assert((v1*v2)==vec2i(3, 3));
+	assert((v1/v2)==vec2i(0, 0));
+	assert((v2/v1)==vec2i(3, 3));
+}
