33cc6708b67e582607b26cfed646365590cfa69c
mmcomando <mmcomando@gmail.com>
2018-01-20 07:27:11 +0100

    File.getModificationTimestamp

---
+++ source/mutils/file.d
@@ -0,0 +1,24 @@
+﻿module mutils.file;
+
+import mutils.string;
+
+
+struct File{
+	static long getModificationTimestamp(string path){
+		version(Posix){
+			import core.sys.posix.sys.stat: stat, stat_t;
+		}else version(Windows){
+			import core.sys.windows.stat: stat, stat_t=struct_stat;
+		}else{
+			static assert(0);
+		}
+		auto tmpCString=getTmpCString(path);
+		stat_t statbuf = void;
+		stat(tmpCString.str.ptr, &statbuf);
+		return statbuf.st_mtime;
+	}
+}
+
+unittest{
+	File.getModificationTimestamp("source/app.d");
+}
---
+++ source/mutils/string.d
@@ -0,0 +1,52 @@
+﻿module mutils.string;
+
+import core.stdc.stdlib;
+import core.stdc.string;
+
+auto getTmpCString(string dstr){
+	static struct CStrTmp{
+		@disable this();
+		@disable this(this);
+
+		const(char)[] str;
+		private bool deleteMem;
+
+		this(const(char)[] str, bool deleteMem){
+			this.str=str;
+			this.deleteMem=deleteMem;
+		}
+
+		~this(){
+			if(!deleteMem){
+				return;
+			}
+			free(cast(void*)str.ptr);
+			str=null;
+		}
+	}
+	if(dstr.length==0){
+		return CStrTmp(cast(const(char)[])"\0", false);
+	}
+	if(dstr[$-1]=='\0'){
+		return CStrTmp(cast(const(char)[])dstr, false);
+	}
+
+	char* mem=cast(char*)malloc(dstr.length+1);
+	memcpy(mem, cast(void*)dstr.ptr, dstr.length);
+	mem[dstr.length]='\0';
+	return CStrTmp(cast(const(char)[])(mem[0..dstr.length+1]), true);
+}
+
+unittest{
+	auto tmpCString=getTmpCString("asdsdd ddd");
+	assert(tmpCString.deleteMem==true);
+	assert(tmpCString.str=="asdsdd ddd\0");
+
+	tmpCString=getTmpCString("asdsdd ddd\0");
+	assert(tmpCString.deleteMem==false);
+	assert(tmpCString.str=="asdsdd ddd\0");
+
+	tmpCString=getTmpCString("");
+	assert(tmpCString.deleteMem==false);
+	assert(tmpCString.str=="\0");
+}
