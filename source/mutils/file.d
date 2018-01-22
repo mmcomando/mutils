48b76fd9013c012b08cc1b533b90c59ddb1c4887
mmcomando <mmcomando@gmail.com>
2018-01-22 07:45:13 +0100

    Software file watcher

--- source/mutils/file.d
+++ source/mutils/file.d
@@ -1,6 +1,8 @@
 ﻿module mutils.file;
 
 import mutils.string;
+import mutils.container.hash_map;
+import mutils.container.vector;
 
 
 struct File{
@@ -14,11 +16,76 @@ struct File{
 		}
 		auto tmpCString=getTmpCString(path);
 		stat_t statbuf = void;
-		stat(tmpCString.str.ptr, &statbuf);
+		int ok=stat(tmpCString.str.ptr, &statbuf);
+		if(ok!=0){
+			return -1;
+		}
 		return statbuf.st_mtime;
 	}
 }
 
 unittest{
-	File.getModificationTimestamp("source/app.d");
+	long timestapm=File.getModificationTimestamp("source/");
+}
+
+struct FileWatcher{
+	@disable this();
+	private this(int){}
+	@disable this(this);
+	__gshared static FileWatcher instance=FileWatcher(1);
+
+	alias EventDelegate=void delegate(string path, const ref WatchedFileInfo info);
+
+	struct WatchedFileInfo{
+		long timestamp;
+		Vector!EventDelegate dels;
+	}
+	HashMap!(Vector!(char), WatchedFileInfo) watchedFiles;
+
+
+	bool watchFile(string path, EventDelegate del){
+		long timestamp=File.getModificationTimestamp(path);
+		WatchedFileInfo* info=&watchedFiles.getInsertDefault( Vector!(char)(cast(char[])path), WatchedFileInfo(timestamp) );
+		info.timestamp=timestamp;
+		info.dels~=del;
+		return timestamp!=-1;
+	}
+
+	bool removeFileFromWatch(string path){
+		return watchedFiles.tryRemove( Vector!(char)(cast(char[])path) );
+	}
+
+	bool removeFileFromWatch(string path, EventDelegate del){
+		WatchedFileInfo noInfo=WatchedFileInfo(-1);
+		WatchedFileInfo* info=&watchedFiles.getDefault( Vector!(char)(cast(char[])path), noInfo );
+		if(info.timestamp==-1){
+			return false;
+		}
+		return info.dels.tryRemoveElement(del);
+	}
+
+	void update(){
+		foreach(ref Vector!char path, ref WatchedFileInfo info; &watchedFiles.byKeyValue){
+			long timestamp=File.getModificationTimestamp(cast(string)path[]);
+			if(info.timestamp==-1 && timestamp==-1){
+				continue;
+			}
+			if(timestamp!=-1 && timestamp<=info.timestamp){
+				continue;
+			}
+			info.timestamp=timestamp;
+			foreach(del; info.dels){
+				del(cast(string)path[], info);
+			}
+		}
+
+	}
+}
+
+unittest{
+	void watch(string path, const ref FileWatcher.WatchedFileInfo info){}
+	FileWatcher.instance.watchFile("source/app.d", &watch);
+	FileWatcher.instance.update();
+	FileWatcher.instance.removeFileFromWatch("source/app.d", &watch);
+	FileWatcher.instance.removeFileFromWatch("source/app.d");
 }
